08c29240c0515d99d1aea999008a04f7389fbc9d
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2017-06-18 17:36:37 +0530

    re adding push notification

---
+++ app/src/main/java/com/aps/iitkconv/services/MyFirebaseInstanceIdService.java
@@ -0,0 +1,25 @@
+package com.aps.iitkconv.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    public MyFirebaseInstanceIdService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
